6d77da718dc4829dede18b47c845721cd5418fbd
Aristides Cruz <aristidescruz02@gmail.com>
2021-12-07 07:37:50 +0100

    Create project structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Language = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
+
+function Navbar() {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          {/*15:58*/}
+          <Language>EN</Language>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Navbar;
